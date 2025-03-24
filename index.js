965ec58bb1bd5fba08de7f39985ff624fc1dce69
joy021 <112099171+joy021@users.noreply.github.com>
2025-03-24 13:50:23 +0530

    LINK KADHI

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
-app.use("https://joyevents-mern-server.onrender.com/api/users", require("./routes/users-route"));
-app.use("https://joyevents-mern-server.onrender.com/api/events",require("./routes/events-route"));
-app.use("https://joyevents-mern-server.onrender.com/api/payments", require("./routes/payments-route"));
-app.use("https://joyevents-mern-server.onrender.com/api/bookings", require("./routes/bookings-route"));
-app.use("https://joyevents-mern-server.onrender.com/api/reports", require("./routes/reports-route"));
+app.use("/api/users", require("./routes/users-route"));
+app.use("/api/events",require("./routes/events-route"));
+app.use("/api/payments", require("./routes/payments-route"));
+app.use("/api/bookings", require("./routes/bookings-route"));
+app.use("/api/reports", require("./routes/reports-route"));
 
 const port = process.env.PORT || 5000; // do not use 3000 because lots of people use 3000 port
 

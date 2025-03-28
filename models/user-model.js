256bc358bfddadb86f857ac5a3cc4c5ab4c2898f
joy021 <112099171+joy021@users.noreply.github.com>
2025-03-28 14:10:43 +0530

    admin changes jevu hatu evu kari didhu

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,3 @@
-/*
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -35,55 +34,5 @@ const userSchema = new mongoose.Schema(
 );
 
 const UserModel = mongoose.model("users", userSchema);
-
-module.exports = UserModel;
-*/
-
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema(
-  {
-    username: { 
-      type: String, 
-      required: true, 
-      unique: true 
-    }, // Added username field for uniqueness
-
-    name: { 
-      type: String, 
-      required: true 
-    },
-
-    email: { 
-      type: String, 
-      required: true, 
-      unique: true 
-    },
-
-    password: { 
-      type: String, 
-      required: true 
-    },
-
-    profilepicture: { 
-      type: String, 
-      default: "" 
-    },
-
-    isAdmin: { 
-      type: Boolean, 
-      default: false 
-    },
-
-    isActive: { 
-      type: Boolean, 
-      default: true 
-    }, 
-
-  },
-  { timestamps: true } // Keeps track of createdAt and updatedAt fields
-);
-
-const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
